7dbffeb5cf3bee7bcec1494c606d60b395b18117
neqqz <neqqz@proton.me>
2026-09-07 01:32:24 +0300

    fix: decompressCert single Read() truncates payloads over ~32KB

--- u_handshake_client.go
+++ u_handshake_client.go
@@ -100,8 +100,8 @@ func (hs *clientHandshakeStateTLS13) decompressCert(m utlsCompressedCertificateM
 	rawMsg[2] = uint8(m.uncompressedLength >> 8)
 	rawMsg[3] = uint8(m.uncompressedLength)
 
-	n, err := decompressed.Read(rawMsg[4:])
-	if err != nil && !errors.Is(err, io.EOF) {
+	n, err := io.ReadFull(decompressed, rawMsg[4:])
+	if err != nil && !errors.Is(err, io.EOF) && !errors.Is(err, io.ErrUnexpectedEOF) {
 		c.sendAlert(alertBadCertificate)
 		return nil, err
 	}
